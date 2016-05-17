ff17f614a3ddbe74ffbe8b74a2c8608a97ff1942
Alex Allen <alex@koan.is>
2016-05-17 18:33:14 +0100

    Include batching

---
+++ MailChimpV3API/Batching/BatchOperation.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace MailChimpV3API.Batching
+{
+    public class BatchOperation
+    {
+        [JsonProperty("method")]
+        public string Method { get; set; }
+
+        [JsonProperty("path")]
+        public string Path { get; set; }
+
+        [JsonProperty("params")]
+        public object Params { get; set; }
+
+        [JsonProperty("body")]
+        public string Body { get; set; }
+
+        [JsonProperty("operation_id")]
+        public string OperationId { get; set; }
+    }
+}
