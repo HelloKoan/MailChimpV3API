2ba1cb5e5150d1e2d2c168020029c4c0688464b7
robotneal <neal.morris@hotmail.co.uk>
2016-05-19 13:00:22 +0100

    Added functions to interface

--- MailChimpV3API/Sections/Campaigns/ICampaignManager.cs
+++ MailChimpV3API/Sections/Campaigns/ICampaignManager.cs
@@ -1,14 +1,21 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MailChimpV3API.Sections.Campaigns
 {
     public interface ICampaignManager
     {
+        Task<IEnumerable<Campaign>> GetAllAsync(CampaignCollectionFilter filter = null, CancellationToken cancellationToken = default(CancellationToken));
+
         Task<CampaignCollection> GetManyAsync(
             CampaignCollectionFilter filter = null,
             CancellationToken cancellationToken = default(CancellationToken));
 
         Task<Campaign> GetSingleAsync(string campaignId, MailChimpFieldFilter filter = null, CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<Campaign> CreateAsync(CreateCampaignModel campaignModel, CancellationToken cancellationToken = default(CancellationToken));
+
+        Campaign Create(CreateCampaignModel campaignModel);
     }
 }
